5b963fbcc2727730da6a41bf5b84629c62f27996
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2024-05-10 19:19:28 +0330

    [fix][feat](deployment)-Stablishing and installing sass, managing package json and creating folder structure of sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import '../sass/styles.scss'
 
 export default function Home() {
   return (
     <>
-    project
+      <div>project</div>
+      <br />
+      <div className="card">MK Card</div>
     </>
-  );
+  )
 }
